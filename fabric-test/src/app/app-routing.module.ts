1f5b55abe18cb4ceaeb43c9c8cd91adc2a874c2a
Eddie <e.otero81@gmail.com>
2018-07-19 01:50:41 -0400

    first angular home page layout

---
+++ fabric-test/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { CreateComponent } from './create/create.component';
+import { ViewComponent } from './view/view.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: CreateComponent
+  },
+  {
+    path: '',
+    component: ViewComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ fabric-test/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CreateComponent } from './create/create.component';
+import { ViewComponent } from './view/view.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    CreateComponent,
+    ViewComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
